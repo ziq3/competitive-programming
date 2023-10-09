5d312edf286150517749a0b88261732772fd321b
tkhoi123 <tuankhoi3011@gmail.com>
2023-10-09 10:12:09 +0700

    s

--- batch/stresstest.bat
+++ batch/stresstest.bat
@@ -1,9 +1,9 @@
 @echo off
 set name=bai1
 for /l %%i in (1, 1, 111111) do (
-    gen.exe>%name%.inp
-    sol.exe
-    brute.exe>%name%.ans<%name%.inp
-    fc %name%.ans %name%.out ||break
+    gen>%name%.inp
+    sol
+    brute>%name%.ans<%name%.inp
+    fc %name%.ans %name%.out ||exit /b
 )
 type %name%.ans
