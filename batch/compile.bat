833bb0f24baef9c06e7d64e213d39f815e430d7b
tkhoi123 <tuankhoi3011@gmail.com>
2023-10-09 16:14:17 +0700

    das

--- batch/compile.bat
+++ batch/compile.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Batch script to compile all C++ files in the current directory and generate executables with the same names
 for %%f in (*.cpp) do (
-    g++ -std=c++11 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover -Wconversion -Wshadow -Wunused-variable -pedantic -Wall -Wextra %%f -o %%~nf.exe
+    g++ -std=c++11 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover %%f -o %%~nf.exe
 )
 pause
