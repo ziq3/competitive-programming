d47579665a970019e3f7d386b6aa94688a3cd171
ziqqq <105137932+tkhoi123@users.noreply.github.com>
2023-10-17 03:23:25 +0700

    Update stresstest.bat

--- batch/stresstest.bat
+++ batch/stresstest.bat
@@ -1,5 +1,8 @@
 @echo off
 set name=bai1
+for %%f in (*.cpp) do (
+    g++ -std=c++11 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover %%f -o %%~nf.exe
+)
 for /l %%i in (1, 1, 111111) do (
     gen>%name%.inp
     sol
