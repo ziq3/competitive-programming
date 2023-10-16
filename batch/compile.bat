5e640b5d735a8bc2976b63862d3e964a23358452
tkhoi123 <tuankhoi3011@gmail.com>
2023-10-17 03:23:58 +0700

    s

--- batch/compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Batch script to compile all C++ files in the current directory and generate executables with the same names
-for %%f in (*.cpp) do (
-    g++ -std=c++11 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover %%f -o %%~nf.exe
-)
-pause
