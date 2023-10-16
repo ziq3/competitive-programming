093c7c7c6a40567812d6b4fc5e18ac53597bdaed
ziqqq <105137932+tkhoi123@users.noreply.github.com>
2023-10-17 01:45:02 +0700

    Update compile.bat

--- batch/compile.bat
+++ batch/compile.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Batch script to compile all C++ files in the current directory and generate executables with the same names
 for %%f in (*.cpp) do (
-    g++ -std=c++11 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover -Wl,--stack,66060288 %%f -o %%~nf.exe
+    g++ -std=c++11 -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover %%f -o %%~nf.exe
 )
 pause
