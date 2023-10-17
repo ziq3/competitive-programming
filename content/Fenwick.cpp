926f277fab2ed9437a511bc98e49999b559511fb
ziqqq <105137932+tkhoi123@users.noreply.github.com>
2023-10-17 15:42:35 +0700

    Update Fenwick.cpp

--- content/Fenwick.cpp
+++ content/Fenwick.cpp
@@ -2,18 +2,18 @@ struct FT
 {
     vector<ll>s;
     FT(ll _n):s(_n) {};
-    void update(ll pos,ll val)
+    void Update(ll pos,ll val)
     {
         for(; pos<sz(s); pos+=pos&-pos)s[pos]+=val;
     }
-    ll query(ll pos)
+    ll Query(ll pos)
     {
         ll res=0;
         for(; pos>0; pos-=pos&-pos)res+=s[pos];
         return res;
     }
-    ll query(ll l,ll r)
+    ll Query(ll l,ll r)
     {
-        return query(r)-query(l-1);
+        return Query(r)-Query(l-1);
     }
 };
