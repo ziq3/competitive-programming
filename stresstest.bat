1869af1be1ae8b3ca0d2260352429cbffed26a13
ziq3 <105137932+ziq3@users.noreply.github.com>
2024-11-01 15:09:37 +0700

    Update stresstest.bat

--- stresstest.bat
+++ stresstest.bat
@@ -1,7 +1,7 @@
 @echo off
 set name=bai1
 for %%f in (*.cpp) do (
-    g++ -std=c++11 -D_GLIBCXX_DEBUG %%f -o %%~nf.exe
+    g++ -D_GLIBCXX_DEBUG %%f -o %%~nf.exe
 )
 for /l %%i in (1, 1, 111111) do (
     gen>%name%.inp
