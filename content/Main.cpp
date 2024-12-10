c59c8d269abf2644bd6bd41edfbb7d0f902caec0
Nguyen Tuan Khoi <105137932+ziq3@users.noreply.github.com>
2024-12-10 16:36:54 +0700

    Update Main.cpp

--- content/Main.cpp
+++ content/Main.cpp
@@ -7,14 +7,20 @@ using namespace std;
 using ll = long long;
 using pi = pair<ll, ll>;
 const ll N=1e5+1;
-ll n;
+ll tc;
+void solve()
+{
+    ll n;
+    cin>>n;
+}
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 #ifdef LOCAL
     freopen(".inp", "r", stdin);
     freopen(".out", "w", stdout);
 #endif
-    cin >> n;
+    cin >> tc;
+    while(tc--)solve(),el;
     return 0;
 }
 
