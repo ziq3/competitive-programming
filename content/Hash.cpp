4ded4da47edc15f1e7ccd6b7e2eafb7552dcaa2d
ziq3 <tuankhoi3011@gmail.com>
2024-12-14 14:27:54 +0700

    a

--- content/Hash.cpp
+++ content/Hash.cpp
@@ -13,7 +13,7 @@ struct HashInterval {
     vector<H> ha, pw;
     HashInterval(string& str) : ha(sz(str)+1), pw(ha) {
         pw[0] = 1;
-        rep(i,0,sz(str))
+        rep(i,0,sz(str)-1)
             ha[i+1] = ha[i] * C + str[i],
             pw[i+1] = pw[i] * C;
     }
