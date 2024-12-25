4f319b0d88742f18b6a182bdd6514c077aeda8a9
Nguyen Tuan Khoi <105137932+ziq3@users.noreply.github.com>
2024-12-25 14:50:25 +0700

    Update Main.cpp

--- content/Main.cpp
+++ content/Main.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+#define db(val) "["#val" = "<<(val)<<"] "
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
 #define el cout << "\n"
 #define sz(x) (ll)(x).size()
 #define all(x) x.begin(), x.end()
