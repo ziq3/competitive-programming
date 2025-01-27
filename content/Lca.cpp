d225eaafeeea521c617128195a4f7916231741ff
Nguyen Tuan Khoi <105137932+ziq3@users.noreply.github.com>
2025-01-27 12:23:56 +0700

    Update Lca.cpp

--- content/Lca.cpp
+++ content/Lca.cpp
@@ -1,29 +1,53 @@
 struct LCA {
-        int T = 0;
-        vi time, path, ret;
-        RMQ<int> rmq;
-
+        typedef vector<int> vi;
+        int LG, n;
+        vector<vi> up;
+        vi h;
         LCA(vector<vi> &C)
-                : time(sz(C))
-                , rmq((dfs(C, 0, -1), ret))
+                : n(sz(C) - 1)
+                , LG(__lg(sz(C) - 1))
+                , h(sz(C))
+                , up(LG + 1, vi(sz(C)))
         {
+                Dfs(1, 1, C);
+                Build();
         }
-        void dfs(vector<vi> &C, int v, int par)
+        void Dfs(int u, int pre, vector<vi> &g)
         {
-                time[v] = T++;
-                for (int y : C[v])
-                        if (y != par) {
-                                path.push_back(v), ret.push_back(time[v]);
-                                dfs(C, y, v);
+                up[0][u] = pre;
+                for (int v : g[u]) {
+                        if (v == pre)
+                                continue;
+                        h[v] = h[u] + 1;
+                        Dfs(v, u, g);
+                }
+        }
+        void Build()
+        {
+                for (int i = 1; i <= LG; ++i) {
+                        for (int j = 1; j <= n; ++j) {
+                                up[i][j] = up[i - 1][up[i - 1][j]];
                         }
+                }
         }
-
-        int lca(int a, int b)
+        int Lca(int u, int v)
         {
-                if (a == b)
-                        return a;
-                tie(a, b) = minmax(time[a], time[b]);
-                return path[rmq.query(a, b)];
+                if (h[u] < h[v])
+                        swap(u, v);
+                int need = h[u] - h[v];
+                for (int j = LG; j >= 0; --j) {
+                        if ((1 << j) & need) {
+                                u = up[j][u];
+                        }
+                }
+                if (u == v)
+                        return u;
+                for (int i = LG; i >= 0; --i) {
+                        if (up[i][u] != up[i][v]) {
+                                u = up[i][u];
+                                v = up[i][v];
+                        }
+                }
+                return up[0][u];
         }
-        //dist(a,b){return depth[a] + depth[b] - 2*depth[lca(a,b)];}
 };
