96cc4772031e67cb80f6dc17915436e56845aa1d
Nguyen Tuan Khoi <105137932+ziq3@users.noreply.github.com>
2025-01-28 12:18:44 +0700

    Update Mint.cpp

--- content/Mint.cpp
+++ content/Mint.cpp
@@ -1,34 +1,36 @@
-const int MOD = 1e9 + 7;
-
 struct Mint {
+    static const int MOD = 1e9 + 7;
     int v;
-    explicit operator int() { return v; }
-    Mint() { v = 0; }
-    Mint(long long _v) { v = _v % MOD; v += (v < 0) * MOD; }
-    bool operator == (const Mint& oth) const { return v == oth.v; }
-};
-Mint& operator += (Mint& a, const Mint& b) {
-    if ((a.v += b.v) >= MOD) a.v -= MOD;
-    return a;
-}
-Mint& operator -= (Mint& a, const Mint& b) {
-    if ((a.v -= b.v) < 0) a.v += MOD;
-    return a;
-}
-Mint operator + (Mint a, Mint b) { return a += b; }
-Mint operator - (Mint a, Mint b) { return a -= b; }
-Mint operator * (const Mint& a, const Mint& b) { return Mint(1LL * a.v * b.v); }
-Mint& operator *= (Mint& a, Mint b) { return a = a * b; }
-Mint pow(Mint a, long long p) {
-    Mint res = 1;
-    while (p) {
-        if (p & 1) res *= a;
-        a *= a;
-        p >>= 1;
+    explicit operator int() const { return v; }
+    Mint() : v(0) {}
+    Mint(int _v) : v(_v % MOD) {
+        if (v < 0) v += MOD;
     }
-    return res;
-}
-Mint inv(Mint a) {
-    return pow(a, MOD - 2);
-}
-Mint operator / (Mint a, Mint b) { return a * inv(b); }
+    bool operator==(const Mint& oth) const { return v == oth.v; }
+    Mint& operator+=(const Mint& oth) {
+        if ((v += oth.v) >= MOD) v -= MOD;
+        return *this;
+    }
+    Mint& operator-=(const Mint& oth) {
+        if ((v -= oth.v) < 0) v += MOD;
+        return *this;
+    }
+    Mint& operator*=(const Mint& oth) {
+        v = (1LL * v * oth.v) % MOD;
+        return *this;
+    }
+    Mint operator+(const Mint& oth) const { Mint res = *this; res += oth; return res; }
+    Mint operator-(const Mint& oth) const { Mint res = *this; res -= oth; return res; }
+    Mint operator*(const Mint& oth) const { return Mint(v * oth.v); }
+    Mint pow(int p) const {
+        Mint res = 1, a = *this;
+        while (p > 0) {
+            if (p & 1) res *= a;
+            a *= a;
+            p >>= 1;
+        }
+        return res;
+    }
+    Mint inv() const { return pow(MOD - 2); }
+    Mint operator/(const Mint& oth) const { return *this * oth.inv(); }
+};
